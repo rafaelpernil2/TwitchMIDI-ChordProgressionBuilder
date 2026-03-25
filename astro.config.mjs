d969b0e091a7f595ec45eab7dc2f554c09938ac4
rafelpernil2 <mysterysynthman@gmail.com>
2026-03-25 01:49:38 +0100

    fix: add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [preact(), tailwind()],
+  adapter: cloudflare()
 });
